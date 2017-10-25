5cf9ef8499039fa976dc5e3f6d02eaefb6fdc6eb
Unknown <carlixxquinteros@gmail.com>
2017-10-25 17:51:54 -0300

    persistencia

---
+++ Proyecto Integrador/src/main/java/persistencia/DaoEspecie.java
@@ -0,0 +1,106 @@
+package persistencia;
+
+import java.util.List;
+
+import javax.persistence.*;
+import modelo.*;
+
+public class DaoEspecie {
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("veterinaria");
+	EntityManager em = emf.createEntityManager();
+	
+	
+	public  DaoEspecie() {
+		
+	}
+	
+	public Boolean crearEspecie(Especie pEspecie )
+	{
+		try {
+				em.getTransaction().begin();
+				em.persist(pEspecie);
+				em.getTransaction().commit();
+				return true;
+		}catch (Exception e) {
+			// TODO: handle exception
+			em.close();			
+			return false;
+		}
+	}
+	
+	public Boolean eliminarEspecie(Especie pEspecie) {
+		
+		try {
+			em.getTransaction().begin();
+			em.remove(pEspecie);
+			em.getTransaction().commit();
+			return true;
+			
+		} catch (Exception e) {
+			
+			// TODO: handle exception
+			em.close();
+			return false;
+			
+		}
+		
+	}
+	
+	public Especie obtenerUnicaEspecie(long id) 
+	{
+		try {
+			Especie pEspecie = new Especie();
+			pEspecie = em.find(Especie.class, id);
+			return pEspecie;
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+	}
+	
+	public List<Especie> obtenerTodasLasEspecies()
+	{
+		try {
+			
+			TypedQuery<Especie> consulta = em.createQuery("SELECT c FROM Especie c", Especie.class);
+			return consulta.getResultList();
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+	}
+	
+	public Boolean modificarEspecie(Especie pEspecie)
+	{
+		try {
+			em.getTransaction().begin();
+			em.merge(pEspecie);
+			em.getTransaction().commit();
+			return true;
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+		
+	}
+	
+	public List<Raza> obtenerMisRazas(Especie pEspecie)
+	{
+		
+		try {
+			TypedQuery<Raza> consulta = em.createQuery("SELECT r FROM Razas r WHERE r.especie.idEspecie = " + pEspecie.getIdEspecie(), Raza.class);
+			return consulta.getResultList();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+		
+	}
+	
+}
+
